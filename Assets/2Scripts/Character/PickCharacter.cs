fcb176e1aa6cfc9e45bf41c92ab4f3de97056733
vqlinh <169137058+linhvuongsmartwave@users.noreply.github.com>
2024-06-21 21:31:19 +0700

    + Pick Character

--- Assets/2Scripts/Character/PickCharacter.cs
+++ Assets/2Scripts/Character/PickCharacter.cs
@@ -7,27 +7,63 @@ public class PickCharacter : MonoBehaviour
     public GameObject male;
     public GameObject feMale;
     public UiPanelDotween ui;
+    public PickCharacter pickCharacter;
+    bool isPick =true;
+
+    private void Awake()
+    {
+        pickCharacter=GetComponent<PickCharacter>();
+        male.SetActive(true);
+        feMale.SetActive(true);
+        Invoke("Wait", 1f);
+    }
     void Start()
     {
-        male.SetActive(false);
-        feMale.SetActive(false);
-        Invoke("Wait",1f);
+        isPick = PlayerPrefs.GetInt("isPick",1)==1;
+        int selectedCharacter = PlayerPrefs.GetInt("male", 0);
+        Debug.Log(isPick);
+        if (isPick)
+        {
+            pickCharacter.gameObject.SetActive(true);
+        }
+        else
+        {
+            pickCharacter.gameObject.SetActive(false);
+            if (selectedCharacter == 1)
+            {
+                male.SetActive(true);
+                feMale.SetActive(false);
+            }
+            else if (selectedCharacter == 2)
+            {
+                male.SetActive(false);
+                feMale.SetActive(true);
+            }
+        }
+
+
+
     }
 
-    void Update()
-    {
 
-    }
 
     public void Male()
     {
-        male.SetActive(true);
+        feMale.SetActive(false);
         ui.PanelFadeOut();
+        isPick = false;
+        PlayerPrefs.SetInt("isPick",0);
+        PlayerPrefs.SetInt("male",1);
+
     }
     public void FeMale()
     {
-        feMale.SetActive(true);
+        male.SetActive(false);
         ui.PanelFadeOut();
+        isPick = false;
+        PlayerPrefs.SetInt("isPick", 0);
+        PlayerPrefs.SetInt("male", 2);
+
     }
 
     void Wait()
