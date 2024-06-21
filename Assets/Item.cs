9b9e0233df43e8053daac83d5dfea22e484cc0cc
vqlinh <169137058+linhvuongsmartwave@users.noreply.github.com>
2024-06-21 07:40:39 +0700

    +

---
+++ Assets/Item.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float moveDistance = 0.5f; // Khoảng cách di chuyển
+    public float moveDuration = 0.5f; // Thời gian di chuyển lên hoặc xuống
+
+    void Start()
+    {
+        // Tạo một tween di chuyển lên xuống liên tục
+        transform.DOMoveY(transform.position.y + moveDistance, moveDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
