b13648ec6fe3489c73c311dd1a2e9b110f283b80
vqlinhSW <linhvuong@smartwave247.com>
2024-06-25 11:44:57 +0700

    + brick anim

--- Assets/2Scripts/Character/Brick.cs
+++ Assets/2Scripts/Character/Brick.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    //Animator anim;
-    //void Start()
-    //{
-    //    anim = GetComponent<Animator>();
-    //}
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(Const.effectEnemy) || collision.gameObject.CompareTag(Const.effectPlayer))
         {
-            //Invoke(nameof(PlayAnim), 0.5f);
-            Destroy(this.gameObject);
+            Invoke(nameof(PlayAnim), 0.5f);
+            //Destroy(this.gameObject);
         }
     }
 
-    //public void DestroyBrick()
-    //{
-    //    Destroy(this.gameObject);
-    //}
+    public void DestroyBrick()
+    {
+        Destroy(this.gameObject);
+    }
 
-    //public void PlayAnim()
-    //{
-    //    anim.SetBool("Destroy", true);
+    public void PlayAnim()
+    {
+        anim.SetBool("Destroy", true);
 
-    //}
+    }
 }
