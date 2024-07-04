ddc44a66634a7608b766623b7dff3513f6ea4ef4
vqlinh <169137058+linhvuongsmartwave@users.noreply.github.com>
2024-07-04 20:20:06 +0700

    + shop

--- Assets/2Scripts/Character/Shop.cs
+++ Assets/2Scripts/Character/Shop.cs
@@ -1,11 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public void BuyBom()
+    public TextMeshProUGUI txtGold;
+
+    public int gold;
+
+    private void Start()
     {
-        
+        Load();
     }
+
+    public void BuyGold(int gold)
+    {
+        this.gold += gold;
+        Save();
+        UpdateGold();
+
+
+    }
+    private void Update()
+    {
+        print("gold : "+gold);
+    }
+
+
+    public void Load()
+    {
+        gold = PlayerPrefs.GetInt("gold", gold);
+        UpdateGold();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("gold", gold);
+        PlayerPrefs.Save();
+
+    }
+
+    public void UpdateGold()
+    {
+        txtGold.text = gold.ToString();
+
+    }
+
+
 }
